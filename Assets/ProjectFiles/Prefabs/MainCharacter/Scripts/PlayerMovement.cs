c90851b24cb1d384cb1c95a1d9ccce20ae9250cd
KGr1sha <krylov.grigory@gmail.com>
2023-08-08 22:06:02 +0300

    player movement with camera rotation

--- Assets/ProjectFiles/Prefabs/MainCharacter/Scripts/PlayerMovement.cs
+++ Assets/ProjectFiles/Prefabs/MainCharacter/Scripts/PlayerMovement.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour, IMovable
 {
+    [Header("References")]
+    [SerializeField] private TextMeshProUGUI speedText;
+    [SerializeField] private Transform orientation;
+    [SerializeField] private Transform cameraTransform;
+
     [SerializeField] private float moveSpeed;
     [SerializeField] private float jumpPower;
-
-    [SerializeField] private TextMeshProUGUI speedText;
 
     private new Rigidbody rigidbody;
 
@@ -20,13 +23,14 @@ public class PlayerMovement : MonoBehaviour, IMovable
     private void Update()
     {
         MeasureSpeed();
+        CalculateLookDirection();
     }
 
     public void Move(Vector2 inputVector)
     {
         float xVel = inputVector.x;
         float zVel = inputVector.y;
-        Vector3 moveDirection = new Vector3(xVel, 0, zVel);
+        Vector3 moveDirection = orientation.right * xVel + orientation.forward * zVel;
 
         rigidbody.AddForce(moveDirection * moveSpeed, ForceMode.Force);
     }
@@ -39,5 +43,15 @@ public class PlayerMovement : MonoBehaviour, IMovable
     private void MeasureSpeed()
     {
         speedText.text = rigidbody.velocity.magnitude.ToString();
+    }
+
+    private void CalculateLookDirection()
+    {
+        Vector3 lookDirection = this.transform.position - cameraTransform.position;
+        lookDirection = new Vector3(lookDirection.x, 0f, lookDirection.z);
+        orientation.forward = lookDirection.normalized;
+
+        Debug.DrawRay(transform.position + Vector3.up * 2, orientation.forward, Color.green);
+        Debug.DrawRay(transform.position + Vector3.up * 2, orientation.right, Color.red);
     }
 }
